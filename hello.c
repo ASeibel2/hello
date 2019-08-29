7c08fc184f7291fe4a2f79079de176c2ea502b95
ASeibel2 <54328554+ASeibel2@users.noreply.github.com>
2019-08-29 11:23:55 -0500

    Create hello.c
    Adding a line to print my major

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Abby Seibel
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Abby\n");
+  printf("Computer Engineering\n");
+
+  return 0;
+}
